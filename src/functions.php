bdfd4013373f749a94f53bb79f059dc75e0a8f6d
Tobias Olry <tobias@tideways-gmbh.com>
2018-04-11 11:46:58 +0200

    change resource type hinting

--- src/functions.php
+++ src/functions.php
@@ -11,106 +11,123 @@ function hdr_init(int $lowestTrackableValue, int $highestTrackableValue, int $si
 
 /**
  * Calculates the required memory size (C) for the given histogram, excluding the PHP overhead for zval and resource representation.
+ * @param resource $hdr
  */
-function hdr_get_memory_size(resource $hdr): int
+function hdr_get_memory_size($hdr): int
 {
 }
 
 /**
  * Estimate the mean of the given histogram.
-*/
-function hdr_mean(resource $hdr): int
+ * @param resource $hdr
+ */
+function hdr_mean($hdr): int
 {
 }
 
 /**
  * Estimate the standard deviation of the given histogram.
+ * @param resource $hdr
  */
-function hdr_stddev(resource $hdr): float
+function hdr_stddev($hdr): float
 {
 }
 
 /**
  * Retrieve the minimal value of the given histogram.
+ * @param resource $hdr
  */
-function hdr_min(resource $hdr): int
+function hdr_min($hdr): int
 {
 }
 
 /**
  * Retrieve the maximum value of the given histogram.
+ * @param resource $hdr
  */
-function hdr_max(resource $hdr): int
+function hdr_max($hdr): int
 {
 }
 
 /**
  * Record a value (integer) for the given histogram.
  * Returns false if the value is larger than the highest_trackable_value and can't be recorded, true otherwise.
+ * @param resource $hdr
  */
-function hdr_record_value(resource $hdr, int $value): bool
+function hdr_record_value($hdr, int $value): bool
 {
 }
 
 /**
  * Record a value (integer) for $count number of times in the given histogram.
  * Returns false if the value is larger than the highest_trackable_value and can't be recorded, true otherwise.
+ * @param resource $hdr
  */
-function hdr_record_values(resource $hdr, int $value, int $count): bool
+function hdr_record_values($hdr, int $value, int $count): bool
 {
 }
 
 /**
  * Record a value in the histogram and backfill based on an expected interval.
- * Records a value in the histogram, will round this value of to a precision at or better than the significant_figure specified at contruction time. This is specifically used for recording latency. If the value is larger than the expected_interval then the latency recording system has experienced co-ordinated omission. This method fills in the values that would have occured had the client providing the load not been blocked.
+ * Records a value in the histogram, will round this value of to a precision at or better than the significant_figure specified at contruction time.
+ * This is specifically used for recording latency. If the value is larger than the expected_interval then the latency recording system has experienced co-ordinated omission.
+ * This method fills in the values that would have occured had the client providing the load not been blocked.
+ * @param resource $hdr
  */
-function hdr_record_corrected_value(resource $hdr, int $value, int $expected_interval): bool
+function hdr_record_corrected_value($hdr, int $value, int $expected_interval): bool
 {
 }
 
 /**
  * Drop all recorded values of the given histogram.
+ * @param resource $hdr
  */
-function hdr_reset(resource $hdr)
+function hdr_reset($hdr)
 {
 }
 
 /**
  * Get the count of recorded values at a specific value (to within the histogram resolution at the value level).
+ * @param resource $hdr
  */
-function hdr_count_at_value(resource $hdr, int $value): int
+function hdr_count_at_value($hdr, int $value): int
 {
 }
 
 /**
  * Get the value at a specific percentile.
+ * @param resource $hdr
  */
-function hdr_value_at_percentile(resource $hdr, float $percentile): int
+function hdr_value_at_percentile($hdr, float $percentile): int
 {
 }
 
 /**
- * Adds all of the values from 'from' to 'to' histogram and create a new histogram as return value. $to will not be modified. Values will be dropped if they around outside of h.lowest_trackable_value and h.highest_trackable_value.
+ * Adds all of the values from 'from' to 'to' histogram and create a new histogram as return value.
+ * $to will not be modified. Values will be dropped if they around outside of h.lowest_trackable_value and h.highest_trackable_value.
  *
+ * @param resource $to
+ * @param resource $from
  * @return resource
  */
-function hdr_add(resource $to, resource $from)
+function hdr_add($to, resource $from)
 {
 }
 
 /**
  * Mutable version of hdr_add, modifiying the $to histogram.
  */
-function hdr_merge_into(resource $to, resource $from): int
+function hdr_merge_into($to, resource $from): int
 {
 }
 
 /**
  * Initializes the basic iterator for given histogram.
  *
+ * @param resource $hdr
  * @return resource
  */
-function hdr_iter_init(resource $hdr)
+function hdr_iter_init($hdr)
 {
 }
 
@@ -119,18 +136,20 @@ function hdr_iter_init(resource $hdr)
  * Iterate to the next value for the iterator. If there are no more values available return false.
  * Returns array with keys value, count_at_index, count_to_index, highest_equivalent_value.
  *
+ * @param resource $iter
  * @return array|false
  */
-function hdr_iter_next(resource $iter)
+function hdr_iter_next($iter)
 {
 }
 
 /**
  * Initializes the percentile iterator for given histogram.
  *
+ * @param resource $hdr
  * @return resource
  */
-function hdr_percentile_iter_init(resource $hdr, int $ticks_per_half_distance)
+function hdr_percentile_iter_init($hdr, int $ticks_per_half_distance)
 {
 }
 
@@ -146,9 +165,10 @@ function hdr_percentile_iter_next(resouce $iter)
 /**
  * Export array into a serializable array format.
  *
+ * @param resource $hdr
  * @return array|false
  */
-function hdr_export(resource $hdr)
+function hdr_export($hdr)
 {
 }
 
